9d7dbc0eced92acd49cbabdf53310d8e7e174ba5
RODRIGO <roirui211@outlook.com>
2025-11-18 17:33:11 -0600

    chore: rebuild web with backend URL (Render)

--- frontend/web_build/flutter_service_worker.js
+++ frontend/web_build/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "56ad7b6616e0c751a7f8bdca12a55dc1",
+const RESOURCES = {"flutter_bootstrap.js": "dcd5143f845a9ee1b865bc6bd47a33de",
 "version.json": "230dcb07859034002dcbb061daaba3b8",
 "index.html": "71d35416f0cebbd6f750305390a41dec",
 "/": "71d35416f0cebbd6f750305390a41dec",
-"main.dart.js": "dc3cb2cf44e6980bd422b805af729233",
+"main.dart.js": "9abeec0c1118224a40993b52a079ae17",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -16,7 +16,7 @@ const RESOURCES = {"flutter_bootstrap.js": "56ad7b6616e0c751a7f8bdca12a55dc1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "fd091ea5bc8f3ccc10f74bd67179fb6a",
 "assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
-"assets/NOTICES": "8ad7646e070001da21d0565d1037ebac",
+"assets/NOTICES": "ce56817321328681f43bec42536f5e26",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/AssetManifest.bin.json": "a1fee2517bf598633e2f67fcf3e26c94",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
